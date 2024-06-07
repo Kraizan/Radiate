8837b0672b57bdbbd5640290616394765ee2d617
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2024-06-08 04:35:47 +0530

    added loading skeletons.

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,10 +1,26 @@
-import { Toggle } from "./toggle";
+import { getRecommened } from "@/lib/recommended-service";
+import { Recommened, RecommenedSkeleton } from "./recommened";
+import { Toggle, ToggleSkeleton } from "./toggle";
 import { Wrapper } from "./wrapper";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const recommened = await getRecommened();
+
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommened data={recommened} />
+      </div>
     </Wrapper>
+  );
+};
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside className="fixed left-0 flex-flex-col w-[70px] lg:w-60 h-full bg-background border-r border-secondary z-50">
+      <ToggleSkeleton />
+      <RecommenedSkeleton />
+    </aside>
   );
 };
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,13 +1,25 @@
 "use client";
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { ToggleSkeleton } from "./toggle";
+import { RecommenedSkeleton } from "./recommened";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const isClient = useIsClient();
   const { collapsed } = useSidebar((state) => state);
+
+  if (isClient === false)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-secondary z-50">
+        <ToggleSkeleton />
+        <RecommenedSkeleton />
+      </aside>
+    );
 
   return (
     <aside
