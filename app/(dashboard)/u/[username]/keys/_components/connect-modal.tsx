436cf5902a68959083e962d164ab2dadb38c71df
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2024-06-19 19:39:26 +0530

    stream keys and livekit integration.

---
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { IngressInput } from "livekit-server-sdk";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { AlertTriangle } from "lucide-react";
+import { ElementRef, useRef, useState, useTransition } from "react";
+import { createIngress } from "@/actions/ingress";
+import { toast } from "sonner";
+
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type ingressType = typeof RTMP | typeof WHIP;
+
+export const ConnectModal = () => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [ingressType, setIngressType] = useState<ingressType>(RTMP);
+
+  const onSubmit = () => {
+    startTransition(() => {
+      createIngress(parseInt(ingressType))
+        .then(() => {
+          toast.success("Ingress created");
+          closeRef.current?.click();
+        })
+        .catch(() => {
+          toast.error("Failed to create ingress");
+        });
+    });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="primary">Generate connection</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate connection</DialogTitle>
+        </DialogHeader>
+        <Select
+          disabled={isPending}
+          value={ingressType}
+          onValueChange={(value) => {
+            setIngressType(value);
+          }}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Ingress type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={RTMP}>RTMP</SelectItem>
+            <SelectItem value={WHIP}>WHIP</SelectItem>
+          </SelectContent>
+        </Select>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the current
+            connection.
+          </AlertDescription>
+        </Alert>
+        <div className="flex justify-between">
+          <DialogClose ref={closeRef} asChild>
+            <Button variant="ghost">Cancel</Button>
+          </DialogClose>
+          <Button disabled={isPending} variant="primary" onClick={onSubmit}>
+            Generate
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
